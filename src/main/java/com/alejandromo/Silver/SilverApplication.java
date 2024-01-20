6889c10278629adbeecf88b09249eb6ef39d5b95
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2024-01-20 13:13:41 +0100

    Start project

---
+++ src/main/java/com/alejandromo/Silver/SilverApplication.java
@@ -0,0 +1,13 @@
+package com.alejandromo.Silver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejandromo/Silver/SilverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandromo.Silver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
