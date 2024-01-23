46490ca34a201023f7641b761255523687c9fa7a
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2024-01-23 13:33:16 +0100

    Update CategoryController
    Get category by id

--- src/main/java/com/alejandromo/controllers/CategoryController.java
+++ src/main/java/com/alejandromo/controllers/CategoryController.java
@@ -1,7 +1,10 @@
 package com.alejandromo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +13,24 @@ import com.alejandromo.models.Category;
 import com.alejandromo.repositories.CategoryRepository;
 
 @RestController
-@RequestMapping(path="/api")
+@RequestMapping(path = "/api")
 public class CategoryController {
 	@Autowired
 	CategoryRepository categoryRepository;
-	
+
 	@GetMapping("/category")
 	public @ResponseBody Iterable<Category> getAllCategories() {
 		return categoryRepository.findAll();
+	}
+
+	@GetMapping("/category/{id}")
+	public ResponseEntity<Category> getById(@PathVariable int id) {
+		Category category = categoryRepository.findById(id).orElse(null);
+		if (category == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(category, HttpStatus.OK);
+		}
 	}
 
 }
